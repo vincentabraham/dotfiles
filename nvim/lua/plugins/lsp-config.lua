20acb2f7d0bfb52e1af9b69e2b315b0bb295826b
vincentabraham <vincent.ahm@gmail.com>
2025-07-27 10:36:06 -0700

    added lsp for c, python

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -15,8 +15,8 @@ return {
      require("mason-lspconfig").setup {
         ensure_installed = {
           "lua_ls",
---          "clangd",
---          "basedpyright",
+          "clangd",
+          "basedpyright",
         }
       }
     end
@@ -26,8 +26,49 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
---      lspconfig.clangd.setup({})
---      lspconfig.basedpyright.setup({})
+      lspconfig.clangd.setup({
+        cmd = { "clangd" },
+        filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+        root_dir = lspconfig.util.root_pattern(
+          ".clangd",
+          ".clang-tidy",
+          ".clang-format",
+          "compile_commands.json",
+          "compile_flags.txt"
+        ),
+        capabilities = {
+          offsetEncoding = { "utf-8", "utf-16" },
+          textDocument = {
+            completion = {
+              editsNearCursor = true,
+            }
+          }
+        },
+      })
+      lspconfig.basedpyright.setup({
+        cmd = { "basedpyright-langserver", "--stdio" },
+        filetypes = { "python" },
+        root_dir = lspconfig.util.root_pattern(
+          "pyproject.toml",
+          "setup.py",
+          "setup.cfg",
+          "requirements.txt",
+          "Pipfile",
+          "pyrightconfig.json",
+          ".git"
+        ),
+        settings = {
+          basedpyright = {
+            analysis = {
+              autoSearchPaths = true,
+              diagnosticMode = "openFilesOnly",
+              useLibraryCodeForTypes = true,
+            },
+            reportMissingSuperCall = false,
+            typeCheckingMode = "standard",
+          }
+        },
+      })
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {}) -- displays documentation
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {}) -- go to def
       vim.keymap.set({'n', 'v'}, '<space>ca', vim.lsp.buf.code_action, {}) -- code action
